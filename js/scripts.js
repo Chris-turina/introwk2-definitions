0bf3911cc9567862b708c5cf6d182703293e7170
Alan Falcon and Chris Turina <alan@>
2017-09-12 13:39:58 -0700

    finish drop downs

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $("li").click(function() {
     $(this).next(".definition").slideToggle();
   });
+
+  $("li").click(function() {
+    $("li").toggleClass("bold");
+  });
 });
